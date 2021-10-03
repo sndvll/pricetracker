fb5290356ab307880193d18768c04e75d00c19dc
Emil Sundvall <emil@sndvll.com>
2021-10-03 14:15:19 +0200

    calculating percentage change from 24-hour price now.

--- src/app/core/utils/asset.utils.ts
+++ src/app/core/utils/asset.utils.ts
@@ -1,11 +1,12 @@
 import {AssetModel, AssetPrice} from '../model';
 
-export const getCurrentPriceChangePercentage = (id: string, prices: AssetPrice[]): number => {
-  return prices.find(price => price.id === id)?.price_change_percentage_24h || 0;
-}
-
 const getCurrentPrice = (id: string, prices: AssetPrice[]): number => {
   return prices.find(price => price.id === id)?.current_price || 0;
+}
+
+const get24hPrice = (id: string, prices: AssetPrice[]): number => {
+  const price = prices.find(price => price.id === id);
+  return (price?.current_price || 0) - (price?.price_change_24h || 0);
 }
 
 const getPrices = (assets: AssetModel[]) => assets.map(asset => asset.price);
@@ -16,9 +17,14 @@ export const getTotalAmount = (assets: AssetModel[]): number => {
     getCurrentPrice(current.id, prices) * current.quantity + value, 0);
 }
 
-export const getTotalPriceChange = (assets: AssetModel[]): number => {
+export const getTotalAmount24h = (assets: AssetModel[]): number => {
   const prices = getPrices(assets);
-  const sum = assets.reduce((value: number, current: AssetModel) =>
-    value + getCurrentPriceChangePercentage(current.id, prices), 0);
-  return sum / assets.length;
+  return assets.reduce((value: number, current: AssetModel): number =>
+    get24hPrice(current.id, prices) * current.quantity + value, 0);
+}
+
+export const getTotalPriceChange = (assets: AssetModel[]): number => {
+  const currentTotalAmount = getTotalAmount(assets);
+  const totalAmount24h = getTotalAmount24h(assets);
+  return ((currentTotalAmount - totalAmount24h) / totalAmount24h) * 100;
 }
